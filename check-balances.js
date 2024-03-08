f9e8e437885ea7f2a6b5d8ee5bbaa072d4001ea5
mgpunk <71784047+mgpunk@users.noreply.github.com>
2024-03-08 18:19:41 +0100

    Update check-balances.js
    file path

--- check-balances.js
+++ check-balances.js
@@ -1,28 +1,40 @@
-const fs = require('fs')
-const ethers = require('ethers')
-require('colors')
+const fs = require('fs');
+const ethers = require('ethers');
+const readline = require('readline');
+require('colors');
 
 const provider = new ethers.providers.WebSocketProvider(
     'wss://go.getblock.io/bbcea3ee1432440d9d550e65eadc0fb5'
-)
+);
 
-const addresses = fs
-    .readFileSync('hits.txt', 'utf8')
-    .split('\n')
-    .map((val) => {
-        return val.split(',')
-    })
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-;(async () => {
+rl.question('Enter the path to hits.txt file: ', async (filePath) => {
+    const addresses = fs
+        .readFileSync(filePath, 'utf8')
+        .split('\n')
+        .map((val) => {
+            return val.split(',');
+        });
+
+    let checkedCount = 0; // Counter for checked addresses
+
     for (let i = 0; i < addresses.length; i++) {
-        const address = addresses[i][0]
-        const balance = await provider.getBalance(address)
+        const address = addresses[i][0];
+        const balance = await provider.getBalance(address);
+
+        checkedCount++; // Increment the counter for each address checked
 
         if (balance.gt(0)) {
-            console.log(address.bgGreen.black, balance.toString().bgGreen.black)
-            console.log('Private Key: '.yellow, addresses[i][1])
+            console.log(`[${checkedCount}]`, address.bgGreen.black, balance.toString().bgGreen.black);
+            console.log('Private Key: '.yellow, addresses[i][1]);
         } else {
-            console.log(address, 0)
+            console.log(`[${checkedCount}]`, address, 0);
         }
     }
-})()
+
+    rl.close();
+});
