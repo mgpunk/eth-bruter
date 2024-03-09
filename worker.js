9207e94726435fae75ed3d8933c9ae432af72b88
mgpunk <71784047+mgpunk@users.noreply.github.com>
2024-03-09 14:50:13 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,12 +2,13 @@ const fs = require("fs");
 const { ethers } = require("ethers");
 
 let tries = 0, hits = 0;
-let currentFileIndex = 1;
+let currentFileIndex = 0; // Start with 0 to include hits.txt
+const hitsPerFile = 1000; // Number of hits per file
 const delay = time => new Promise(res => setTimeout(res, time));
 const words = fs.readFileSync("bip39.txt", { encoding: 'utf8', flag: 'r' }).replace(/(\r)/gm, "").toLowerCase().split("\n");
 const usedMnemonics = new Set();
 
-function gen12(words) {
+function gen24(words) {
     const n = 24;
     const shuffled = words.slice(); // Create a copy of the array
     for (let i = shuffled.length - 1; i > 0; i--) {
@@ -24,16 +25,18 @@ async function doCheck() {
     try {
         let mnemonic;
         do {
-            mnemonic = gen12(words);
+            mnemonic = gen24(words);
         } while (usedMnemonics.has(mnemonic));
         
         usedMnemonics.add(mnemonic);
 
         const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-        const filename = `hits.${currentFileIndex}.txt`;
+        const filename = currentFileIndex === 0 ? 'hits.txt' : `hits.${currentFileIndex}.txt`;
         fs.appendFileSync(filename, wallet.address + "," + wallet.privateKey + "\n");
         process.stdout.write("+");
-        if (++hits % 10000 === 0) {
+        hits++;
+        console.log(`Hits: ${hits}, Current File Index: ${currentFileIndex}, File Name: ${filename}`);
+        if (hits % hitsPerFile === 0) {
             currentFileIndex++;
         }
     } catch (e) {
@@ -41,7 +44,7 @@ async function doCheck() {
     }
     await delay(0); // Prevent Call Stack Overflow
     process.stdout.write("-");
-    if (hits >= 100000000) { // End the process after 100000 hits
+    if (hits >= 1000000) { // End the process after 100000 hits
         console.log("Process completed.");
         process.exit(0);
     }
@@ -49,4 +52,3 @@ async function doCheck() {
 }
 
 doCheck();
-
